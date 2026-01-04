a6c1a132e29e0482ad43b8beae8bd0233bfd0512
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-04 20:23:47 +0545

    Setup Tailwind CSS and update React app styles

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('talwindcss').Config}*/
+
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+
+    theme: {
+        extend: {}
+    }
+}
